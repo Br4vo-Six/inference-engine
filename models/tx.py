6076c0a5fc07030b996dc06c06db45eaccaf5b0e
waffiq <waffiqmaaroja1998@gmail.com>
2024-07-27 13:21:01 +0700

    Change model field

--- models/tx.py
+++ models/tx.py
@@ -4,44 +4,55 @@ from datetime import datetime
 
 
 class Input(BaseModel):
-    prev_hash: Optional[str] = Field(None, description="Previous transaction hash")
+    prev_hash: Optional[str] = Field(
+        None, description="Previous transaction hash")
     output_index: Optional[int] = Field(None, description="Output index")
     output_value: Optional[int] = Field(None, description="Output value")
-    addresses: Optional[List[str]] = Field(None, description="List of addresses")
-    script_type: Optional[str] = Field(None, description="Script type")
+    addresses: Optional[List[str]] = Field(
+        None, description="List of addresses")
+    # script_type: Optional[str] = Field(None, description="Script type")
     age: Optional[int] = Field(None, description="Age of the transaction")
 
     class Config:
-        extra = 'ignore' 
+        extra = 'ignore'
+
 
 class Output(BaseModel):
     value: Optional[int] = Field(None, description="Output value")
-    script: Optional[str] = Field(None, description="Script")
-    spent_by: Optional[str] = Field(None, description="Transaction that spent this output")
-    addresses: Optional[List[str]] = Field(None, description="List of addresses")
-    script_type: Optional[str] = Field(None, description="Script type")
+    # script: Optional[str] = Field(None, description="Script")
+    spent_by: Optional[str] = Field(
+        None, description="Transaction that spent this output")
+    addresses: Optional[List[str]] = Field(
+        None, description="List of addresses")
+    # script_type: Optional[str] = Field(None, description="Script type")
 
     class Config:
-        extra = 'ignore' 
+        extra = 'ignore'
+
 
 class Tx(BaseModel):
     hash: str = Field(description="Transaction hash")
-    block_hash: Optional[str] = Field(None, description="Block hash")
+    # block_hash: Optional[str] = Field(None, description="Block hash")
     block_height: Optional[int] = Field(None, description="Block height")
-    addresses: Optional[List[str]] = Field(None, description="List of addresses")
+    addresses: Optional[List[str]] = Field(
+        None, description="List of addresses")
     total: Optional[int] = Field(None, description="Total amount")
     fees: Optional[int] = Field(None, description="Transaction fees")
     size: Optional[int] = Field(None, description="Transaction size")
     vsize: Optional[int] = Field(None, description="Virtual transaction size")
-    confirmed: Optional[datetime] = Field(None, description="Confirmation timestamp")
-    received: Optional[datetime] = Field(None, description="Reception timestamp")
-    ver: Optional[int] = Field(None, description="Transaction version")
-    double_spend: Optional[bool] = Field(None, description="Double spend flag")
+    confirmed: Optional[datetime] = Field(
+        None, description="Confirmation timestamp")
+    # received: Optional[datetime] = Field(None, description="Reception timestamp")
+    # ver: Optional[int] = Field(None, description="Transaction version")
+    # double_spend: Optional[bool] = Field(None, description="Double spend flag")
     vin_sz: Optional[int] = Field(None, description="Number of inputs")
     vout_sz: Optional[int] = Field(None, description="Number of outputs")
     inputs: Optional[List[Input]] = Field(None, description="List of inputs")
-    outputs: Optional[List[Output]] = Field(None, description="List of outputs")
-    licit: Optional[bool] = Field(default=None, description="Is the transaction licit or illicit?") # True if tx is licit, False if illicit
+    outputs: Optional[List[Output]] = Field(
+        None, description="List of outputs")
+    # True if tx is licit, False if illicit
+    licit: Optional[bool] = Field(
+        default=None, description="Is the transaction licit or illicit?")
 
     class Config:
-        extra = 'ignore' 
+        extra = 'ignore'
--- models/wallet.py
+++ models/wallet.py
@@ -2,21 +2,25 @@ from pydantic import BaseModel, Field
 from typing import List, Optional
 from datetime import datetime
 
+
 class TxRef(BaseModel):
     tx_hash: str
     block_height: Optional[int] = Field(default=None)
     tx_input_n: Optional[int] = Field(default=0)
     tx_output_n: Optional[int] = Field(default=0)
     value: Optional[int] = Field(default=0)
-    ref_balance: Optional[int] = Field(default=None)
+    # ref_balance: Optional[int] = Field(default=None)
     confirmed: Optional[datetime] = Field(default=False)
-    double_spend: Optional[bool] = Field(default=None)
+    # double_spend: Optional[bool] = Field(default=None)
     spent: Optional[bool] = Field(default=False)
     spent_by: Optional[str] = Field(default=None)
-    licit: Optional[bool] = Field(default=None, description="Is the transaction licit or illicit?") # True if tx is licit, False if illicit
+    # True if tx is licit, False if illicit
+    licit: Optional[bool] = Field(
+        default=None, description="Is the transaction licit or illicit?")
 
     class Config:
-        extra = 'ignore' 
+        extra = 'ignore'
+
 
 class Wallet(BaseModel):
     address: str
@@ -27,4 +31,4 @@ class Wallet(BaseModel):
     txrefs: Optional[List[TxRef]] = Field(default=[])
 
     class Config:
-        extra = 'ignore' 
+        extra = 'ignore'
